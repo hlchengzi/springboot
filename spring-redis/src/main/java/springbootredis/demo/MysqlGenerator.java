fd1c80a61f6601c9fed00a50b11b63fa458e1b45
xuxiangcheng <xuxiangcheng@zuoyebang.com>
2019-08-01 15:29:46 +0800

    Mybatis-PLUS 生成器

---
+++ spring-redis/src/main/java/springbootredis/demo/MysqlGenerator.java
@@ -0,0 +1,135 @@
+package springbootredis.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.FileOutConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+
+public class MysqlGenerator {
+
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    /**
+     * RUN THIS
+     */
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        // String projectPath = System.getProperty("D:/WorkSpace/zyb/zyb-dcs");
+        String path = "D:/projrcts/spring-demo/spring-redis";
+        gc.setOutputDir(path + "/src/main/java");
+        gc.setAuthor("xcxu");
+        gc.setOpen(false);
+
+        gc.setFileOverride(true);
+        gc.setActiveRecord(true);// 不需要ActiveRecord特性的请改为false
+        gc.setEnableCache(false);// XML 二级缓存
+        gc.setBaseResultMap(true);// XML ResultMap
+        gc.setBaseColumnList(true);// XML columList
+
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://localhost:3306/test?useUnicode=true&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("123456");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+       // pc.setModuleName(scanner("模块名"));
+        pc.setParent("springbootredis.demo");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        List<FileOutConfig> focList = new ArrayList<>();
+        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                String path = "D:/projrcts/spring-demo/spring-redis";
+
+                // 自定义输入文件名称
+                return path + "src/main/resources/"+"/mapper/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        TemplateConfig templateConfig = new TemplateConfig();
+        mpg.setTemplate(templateConfig.setXml(null));
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        // strategy.setSuperEntityClass("com.zybang.lip.ezs.ps.entity.SuperEntity");
+        strategy.setEntityLombokModel(true);
+        // strategy.setSuperControllerClass("com.zybang.lip.ezs.core.base.BaseController");
+        // strategy.setInclude(scanner("表名"));
+
+        strategy.setInclude(scanner("表名").split(","));
+
+        // 排除需要生成的表，与 include 只能保留一项，默认生成全部
+        // strategy.setExclude(new String[]{"sys_resource"});
+
+        // strategy.setSuperEntityColumns("id");
+        strategy.setControllerMappingHyphenStyle(true);
+
+//        // 生成实体去掉表前缀
+//        // strategy.setTablePrefix(pc.getModuleName() + "_");
+//        strategy.setTablePrefix(new String[] { "zyb_", "sys_", "tb_", "t_", "sk_" });
+//        // strategy.setTablePrefix(pc.getModuleName() + "_");
+
+        mpg.setStrategy(strategy);
+        // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+
+    }
+
+}
